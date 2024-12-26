7610b328774cda70abc6edb1bc87f272a100ebc8
Axyl07 <aryanchauhan000777@gmail.com>
2024-12-26 15:27:03 +0530

    Fix (works for 1080p and 100% zoom only)

--- src/image-carousel.js
+++ src/image-carousel.js
@@ -4,12 +4,13 @@ export default function carousel() {
   const backward = document.querySelector(".backward");
   const slides = document.querySelector(".slides");
   slides.style.position = "absolute";
-
+    // const currentPosDiv = document.querySelector('.currentpos');
   forward.addEventListener("click", () => {
     const currentPosition = slides.getBoundingClientRect();
-    let currentLeft = parseInt(currentPosition.left);
+      let currentLeft = parseInt(currentPosition.left);
+    //   currentPosDiv.textContent = currentLeft;
     console.log(currentLeft);
-    if (currentLeft > -1094) {
+    if (currentLeft >= -290) {
       slides.style.left = currentLeft - 360 + "px";
       console.log("forward");
       console.log(slides.style.left);
@@ -19,9 +20,10 @@ export default function carousel() {
   });
   backward.addEventListener("click", () => {
     const currentPosition = slides.getBoundingClientRect();
-    let currentLeft = parseInt(currentPosition.left);
+      let currentLeft = parseInt(currentPosition.left);
+    //   currentPosDiv.textContent = currentLeft;
     console.log(currentLeft);
-    if (currentLeft <157) {
+    if (currentLeft <790) {
       slides.style.left = currentLeft + 360 + "px";
       console.log(slides.style.right);
       console.log("backward");
