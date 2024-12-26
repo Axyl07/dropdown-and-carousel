35ae31d4db224f3a1430cd1f08c1d3d3810d405c
Axyl07 <aryanchauhan000777@gmail.com>
2024-12-26 19:21:23 +0530

    Added overflow:hidden to only show one image at a time

--- src/image-carousel.js
+++ src/image-carousel.js
@@ -1,37 +1,41 @@
 import "./image-carousel.css";
 export default function carousel() {
- setInterval(() => {
-    moveForward()
- }, 5000);
+//  setInterval(() => {
+//     moveForward()
+//  }, 5000);
     
   const forward = document.querySelector(".forward");
   const backward = document.querySelector(".backward");
-  const slides = document.querySelector(".slides");
+    const slides = document.querySelector(".slides");
+    const test = document.querySelector('.currentpos');
+    const currentPosition = slides.getBoundingClientRect();
+    let currentLeft = parseInt(currentPosition.left);
+    test.textContent = currentLeft;
+    slides.style.left = currentLeft - 790 + 'px';
+    test.textContent = slides.style.left
+
 
   const moveForward = function () {
-    const currentPosition = slides.getBoundingClientRect();
-    let currentLeft = parseInt(currentPosition.left);
-    console.log(currentLeft);
-    if (currentLeft >= -290) {
-      slides.style.left = currentLeft - 360 + "px";
-      console.log("forward");
-      console.log(slides.style.left);
-    } else {
-      console.log("last image");
-    }
+      // const currentPosition = slides.getBoundingClientRect();
+      let currentLeft = parseInt(slides.style.left);
+      if (currentLeft >= -1080) {
+          test.textContent = currentLeft;
+          slides.style.left = currentLeft - 360 + 'px';
+          console.log(currentLeft);
+          test.textContent = currentLeft;  
+      } else {
+          console.log('lastt image');
+      }
   };
 
-  const moveBackward = function () {
-    const currentPosition = slides.getBoundingClientRect();
-    let currentLeft = parseInt(currentPosition.left);
-    console.log(currentLeft);
-    if (currentLeft < 790) {
-      slides.style.left = currentLeft + 360 + "px";
-      console.log(slides.style.right);
-      console.log("backward");
-    } else {
-      console.log("First image");
-    }
+  const moveBackward = function () { 
+    let currentLeft = parseInt(slides.style.left);
+      if (currentLeft < 0) {
+          test.textContent = currentLeft;
+          slides.style.left = currentLeft + 360 + 'px';
+          console.log(currentLeft);
+          test.textContent = currentLeft;
+      } else console.log('first image');
   };
   forward.addEventListener("click", () => {
     moveForward();
