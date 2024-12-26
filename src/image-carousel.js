c37457178ab1232000a7635167894e0e8223dfde
Axyl07 <aryanchauhan000777@gmail.com>
2024-12-26 15:12:51 +0530

    Prevent overflowing after first and last image

--- src/image-carousel.js
+++ src/image-carousel.js
@@ -1,28 +1,32 @@
+import "./image-carousel.css";
 export default function carousel() {
- 
+  const forward = document.querySelector(".forward");
+  const backward = document.querySelector(".backward");
+  const slides = document.querySelector(".slides");
+  slides.style.position = "absolute";
 
-    const forward = document.querySelector('.forward');
-    const backward = document.querySelector('.backward');
-    const slides = document.querySelector('.slides');
-    slides.style.position = 'absolute';
-    
-    
-    forward.addEventListener('click', () => {
-        const currentPosition = slides.getBoundingClientRect();
-        let currentLeft = parseInt(currentPosition.left);
-        console.log(currentLeft);
-        slides.style.left = currentLeft - 360 + 'px';
-        console.log('forward');
-        console.log(slides.style.left)
-    })
-    backward.addEventListener('click', () => {
-        const currentPosition = slides.getBoundingClientRect();
-        let currentLeft = parseInt(currentPosition.left);
-        console.log(currentLeft);
-        slides.style.left = currentLeft + 360 + 'px';
-        console.log(slides.style.right);
-        console.log('backward');
-
-
-    })
+  forward.addEventListener("click", () => {
+    const currentPosition = slides.getBoundingClientRect();
+    let currentLeft = parseInt(currentPosition.left);
+    if (currentLeft > -923) {
+      console.log(currentLeft);
+      slides.style.left = currentLeft - 360 + "px";
+      console.log("forward");
+      console.log(slides.style.left);
+    } else {
+      console.log("last image");
+    }
+  });
+  backward.addEventListener("click", () => {
+    const currentPosition = slides.getBoundingClientRect();
+    let currentLeft = parseInt(currentPosition.left);
+    console.log(currentLeft);
+    if (currentLeft < 157) {
+      slides.style.left = currentLeft + 360 + "px";
+      console.log(slides.style.right);
+      console.log("backward");
+    } else {
+      console.log("First image");
+    }
+  });
 }
