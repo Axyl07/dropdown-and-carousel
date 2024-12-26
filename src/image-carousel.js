1dfe68a98492d65d9ee2cd973e56c7d8ce96d4f9
Axyl07 <aryanchauhan000777@gmail.com>
2024-12-26 15:01:40 +0530

    Add image moving functionality in carousel

--- src/image-carousel.js
+++ src/image-carousel.js
@@ -1,14 +1,28 @@
 export default function carousel() {
+ 
+
     const forward = document.querySelector('.forward');
     const backward = document.querySelector('.backward');
     const slides = document.querySelector('.slides');
+    slides.style.position = 'absolute';
+    
+    
     forward.addEventListener('click', () => {
-        slides.style.position = 'absolute';
-        slides.style.left = '360px';
+        const currentPosition = slides.getBoundingClientRect();
+        let currentLeft = parseInt(currentPosition.left);
+        console.log(currentLeft);
+        slides.style.left = currentLeft - 360 + 'px';
         console.log('forward');
+        console.log(slides.style.left)
     })
     backward.addEventListener('click', () => {
-        slides.style.transform = 'translateX(360px)';
+        const currentPosition = slides.getBoundingClientRect();
+        let currentLeft = parseInt(currentPosition.left);
+        console.log(currentLeft);
+        slides.style.left = currentLeft + 360 + 'px';
+        console.log(slides.style.right);
         console.log('backward');
+
+
     })
 }
