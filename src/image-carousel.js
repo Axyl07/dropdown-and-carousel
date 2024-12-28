46cb025a62f75adf33d1fc2823d8341d236c55d1
Axyl07 <aryanchauhan000777@gmail.com>
2024-12-28 20:48:12 +0530

    Complete jump button functionality

--- src/image-carousel.js
+++ src/image-carousel.js
@@ -1,50 +1,161 @@
 import "./image-carousel.css";
 export default function carousel() {
 
-        // const c1 = document.querySelector('#one');
-        // c1.style.backgroundColor = 'black';
 
+  // setInterval(() => {
+  //   moveForward();
+  // }, 5000);
 
-
-    setInterval(() => {
-    moveForward()
- }, 5000);
-    
-    
   const forward = document.querySelector(".forward");
   const backward = document.querySelector(".backward");
-    const slides = document.querySelector(".slides");
-    const test = document.querySelector('.currentpos');
-    const currentPosition = slides.getBoundingClientRect();
-    let currentLeft = parseInt(currentPosition.left);
-    test.textContent = currentLeft;
-    slides.style.left = currentLeft - 790 + 'px';
-    test.textContent = slides.style.left
+  const slides = document.querySelector(".slides");
+  const test = document.querySelector(".currentpos");
+  const currentPosition = slides.getBoundingClientRect();
+  let currentLeft = parseInt(currentPosition.left);
+  // test.textContent = currentLeft;
+  slides.style.left = currentLeft - 790 + "px";
+  // test.textContent = slides.style.left;
+
+  const circleBtns = document.querySelectorAll(".jumpCircles > input");
+  console.log(circleBtns);
+  //radio event listener
+  circleBtns.forEach((button) => {
+    button.addEventListener('click', () => {
+      sliderJump();
+    })
+  })
 
 
-    const moveForward = function () {
 
-      // const currentPosition = slides.getBoundingClientRect();
-      let currentLeft = parseInt(slides.style.left);
-      if (currentLeft >= -1080) {
-          test.textContent = currentLeft;
-          slides.style.left = currentLeft - 360 + 'px';
-          console.log(currentLeft);
-          test.textContent = currentLeft;  
-      } else {
-          console.log('lastt image');
+  const sliderJump = function () {
+    circleBtns.forEach((btn) => {
+      if (btn.checked === true) {
+        test.textContent = btn.id;
+        const currentPosition = slides.getBoundingClientRect();
+        let currentLeft = parseInt(currentPosition.left);
+        slides.style.left = currentLeft - 790 + "px";
+        let startingPosition = slides.style.left;
+        test.textContent += " "+startingPosition;
+        if (startingPosition==='0px') {
+          if (btn.id==='two') {
+            let currentLeft = parseInt(slides.style.left);
+            slides.style.left = currentLeft - 360 + "px";
+          }
+          if (btn.id==='three') {
+            let currentLeft = parseInt(slides.style.left);
+            slides.style.left = currentLeft - 720 + "px";
+          }
+          if (btn.id==='four') {
+            let currentLeft = parseInt(slides.style.left);
+            slides.style.left = currentLeft - 1080 + "px";
+          }
+          if (btn.id==='five') {
+            let currentLeft = parseInt(slides.style.left);
+            slides.style.left = currentLeft - 1440 + "px";
+          }
+        }
+        if (startingPosition==='-360px') {
+          if (btn.id==='one') {
+            let currentLeft = parseInt(slides.style.left);
+            slides.style.left = currentLeft + 360 + "px";
+          }
+          if (btn.id==='three') {
+            let currentLeft = parseInt(slides.style.left);
+            slides.style.left = currentLeft - 360 + "px";
+          }
+          if (btn.id==='four') {
+            let currentLeft = parseInt(slides.style.left);
+            slides.style.left = currentLeft - 720 + "px";
+          }
+          if (btn.id==='five') {
+            let currentLeft = parseInt(slides.style.left);
+            slides.style.left = currentLeft - 1080 + "px";
+          }
+        }
+        if (startingPosition==='-720px') {
+          if (btn.id==='one') {
+            let currentLeft = parseInt(slides.style.left);
+            slides.style.left = currentLeft + 720 + "px";
+          }
+          if (btn.id==='two') {
+            let currentLeft = parseInt(slides.style.left);
+            slides.style.left = currentLeft + 360 + "px";
+          }
+          if (btn.id==='four') {
+            let currentLeft = parseInt(slides.style.left);
+            slides.style.left = currentLeft - 360 + "px";
+          }
+          if (btn.id==='five') {
+            let currentLeft = parseInt(slides.style.left);
+            slides.style.left = currentLeft - 720 + "px";
+          }
+        }
+        if (startingPosition==='-1080px') {
+          if (btn.id==='one') {
+            let currentLeft = parseInt(slides.style.left);
+            slides.style.left = currentLeft + 1080 + "px";
+          }
+          if (btn.id==='two') {
+            let currentLeft = parseInt(slides.style.left);
+            slides.style.left = currentLeft + 720 + "px";
+          }
+          if (btn.id==='three') {
+            let currentLeft = parseInt(slides.style.left);
+            slides.style.left = currentLeft + 360 + "px";
+          }
+          if (btn.id==='five') {
+            let currentLeft = parseInt(slides.style.left);
+            slides.style.left = currentLeft - 360 + "px";
+          }
+        }
+        if (startingPosition==='-1440px') {
+          if (btn.id==='one') {
+            let currentLeft = parseInt(slides.style.left);
+            slides.style.left = currentLeft + 1440 + "px";
+          }
+          if (btn.id==='two') {
+            let currentLeft = parseInt(slides.style.left);
+            slides.style.left = currentLeft + 1080 + "px";
+          }
+          if (btn.id==='three') {
+            let currentLeft = parseInt(slides.style.left);
+            slides.style.left = currentLeft + 720 + "px";
+          }
+          if (btn.id==='four') {
+            let currentLeft = parseInt(slides.style.left);
+            slides.style.left = currentLeft + 360 + "px";
+          }
+        }
+  
       }
+    });
+    
+  }
+  sliderJump();
+
+  const moveForward = function () {
+    // const currentPosition = slides.getBoundingClientRect();
+    let currentLeft = parseInt(slides.style.left);
+    if (currentLeft >= -1080) {
+      // test.textContent = currentLeft;
+      slides.style.left = currentLeft - 360 + "px";
+      console.log(currentLeft);
+      test.textContent = slides.style.left;
+    } else {
+      console.log("lastt image");
+    }
   };
 
-  const moveBackward = function () { 
+  const moveBackward = function () {
     let currentLeft = parseInt(slides.style.left);
-      if (currentLeft < 0) {
-          test.textContent = currentLeft;
-          slides.style.left = currentLeft + 360 + 'px';
-          console.log(currentLeft);
-          test.textContent = currentLeft;
-      } else console.log('first image');
+    if (currentLeft < 0) {
+      // test.textContent = currentLeft;
+      slides.style.left = currentLeft + 360 + "px";
+      console.log(currentLeft);
+      test.textContent = slides.style.left;
+    } else console.log("first image");
   };
+
   forward.addEventListener("click", () => {
     moveForward();
   });
@@ -52,18 +163,3 @@ export default function carousel() {
     moveBackward();
   });
 }
-
-
-const circleBtns = document.querySelectorAll('.jumpCircles > button');
-circleBtns.forEach((btn) => {
-  btn.addEventListener('click', () => {
-    if (btn.style.backgroundColor!='black') {
-      btn.style.backgroundColor = 'black';
-      
-    } else {
-      btn.style.backgroundColor = 'white';
-    }
-  
-    
-  })
-})
